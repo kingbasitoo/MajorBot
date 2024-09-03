6e5986cba2129dd09bd968057e3db3f15f7c3775
Basit Khan <123544842+kingbasitoo@users.noreply.github.com>
2024-09-03 12:39:39 +0500

    config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     API_ID: 28354723
     API_HASH: 41e7ce41a7b75cc2a109133bb4d73156
    
-    REF_ID: str = '339631649'
+    REF_ID: str = '7240829185'
     SUBSCRIBE_HIDDEN_CODE: bool = True
     USE_RANDOM_DELAY_IN_RUN: bool = True
     RANDOM_DELAY_IN_RUN: list[int] = [0, 15]
